cb03452b2c76f55cf7eed8cf381beb2da34a2afa
jehanjoshi <jehan.joshi@skaps.com>
2020-02-29 15:11:19 -0500

    Postgres and Python

---
+++ RelationalDatabases/PytonAndPostgres.py
@@ -0,0 +1,28 @@
+# To interact with Postgresql use the following package
+
+import psycopg2
+
+# Create a connection and cursor to the databases
+
+def Connect():
+
+    try:
+        conn = psycopg2.connect("host= 192.168.1.1 dbname=Test user=admin password=admin ")
+    except psycopg2.Error as e:
+        print("couldnot connect")
+
+    try:
+        cur = conn.cursor()
+    except psycopg2.Error as e:
+        print("cursor failed")
+
+    conn.set_session(autocommit=True)
+    return conn,cur
+
+#insert value into a created table music_store2
+
+def insert_value(conn,cur):
+
+    cur.execute("INSERT INTO music_store2 (transaction_id,customer_name,cashier_name,year,albums_purchased) \
+                 VALUES (%s, %s, %s, %s, %s)", \
+                 (1, "v1", "Sam", 2000, ["Rubber Soul", "Let it Be"]))
